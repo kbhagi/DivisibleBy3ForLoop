7a0e83774d1f9619fe7c1786b9d75611f5e01e91
bhargavak <19756773+kbhagi@users.noreply.github.com>
2020-03-15 11:05:17 +0530

    Add files via upload

---
+++ DivisibleBy3ForLoop/DivisibleBy3ForLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DivisibleBy3ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Following are the numbers that are  divisible by 3:");
+            for (int i = 1; i <= 100; i++)
+            {
+                if ((i % 3) == 0)
+                {
+                    Console.Write("{0} ", i);
+                    count++;
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.WriteLine("From 1 to 100 total {0} numbers are divisible by 3", count);
+            Console.ReadLine();
+        }
+    }
+}
+
